c4d399098bddfdf6d33d50cc67c03de2e1b368ea
Kyle Lange <kyletaylorlange@gmail.com>
2025-02-16 00:30:13 -0800

    Section 2

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,18 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
     bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+    Super::PlayerTick(DeltaTime);
+
+    CursorTrace();
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -48,5 +56,31 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
     {
         ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
         ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+    }
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+    FHitResult CursorHit;
+    GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, CursorHit);
+    if (!CursorHit.bBlockingHit)
+    {
+        return;
+    }
+
+    LastActor = ThisActor;
+    ThisActor = CursorHit.GetActor();
+
+    // Redo highlighting if the actor has changed since last frame.
+    if (LastActor != ThisActor)
+    {
+        if (LastActor)
+        {
+            LastActor->UnhighlightActor();
+        }
+        if (ThisActor)
+        {
+            ThisActor->HighlightActor();
+        }
     }
 }
